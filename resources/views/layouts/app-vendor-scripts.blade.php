816d0252441adf495b26509ed4a5340505e16078
Royan Harsha <royan@konekt.lk>
2023-10-15 02:45:42 +0530

    Add : Theme Setup

---
+++ resources/views/layouts/app-vendor-scripts.blade.php
@@ -0,0 +1,9 @@
+<script type="text/javascript" src="{{ URL::asset('/build/bower_components/jquery/js/jquery.min.js') }}"></script>
+<script type="text/javascript" src="{{ URL::asset('/build/bower_components/jquery-ui/js/jquery-ui.min.js') }}"></script>
+<script type="text/javascript" src="{{ URL::asset('/build/bower_components/popper.js/js/popper.min.js') }}"></script>
+<script type="text/javascript" src="{{ URL::asset('/build/bower_components/bootstrap/js/bootstrap.min.js') }}"></script>
+
+<script src="{{ URL::asset('/build/assets/pages/waves/js/waves.min.js') }}"></script>
+
+<script type="text/javascript" src="{{ URL::asset('/build/bower_components/jquery-slimscroll/js/jquery.slimscroll.js') }}"></script>
+@yield('script')
---
+++ resources/views/layouts/vendor-scripts.blade.php
@@ -0,0 +1,15 @@
+    <script type="text/javascript" src="{{ URL::asset('/build/bower_components/jquery/js/jquery.min.js') }}"></script>
+    <script type="text/javascript" src="{{ URL::asset('/build/bower_components/jquery-ui/js/jquery-ui.min.js') }}"></script>
+    <script type="text/javascript" src="{{ URL::asset('/build/bower_components/popper.js/js/popper.min.js') }}"></script>
+    <script type="text/javascript" src="{{ URL::asset('/build/bower_components/bootstrap/js/bootstrap.min.js') }}"></script>
+
+    <script src="{{ URL::asset('/build/assets/pages/waves/js/waves.min.js') }}"></script>
+
+    <script type="text/javascript" src="{{ URL::asset('/build/bower_components/jquery-slimscroll/js/jquery.slimscroll.js') }}"></script>
+
+    <script type="text/javascript" src="{{ URL::asset('/build/bower_components/modernizr/js/modernizr.js') }}"></script>
+    <script type="text/javascript" src="{{ URL::asset('/build/bower_components/modernizr/js/css-scrollbars.js') }}">
+    </script>
+    <script type="text/javascript" src="{{ URL::asset('/build/assets/js/common-pages.js') }}"></script>
+
+    @yield('script')
