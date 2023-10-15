c280ca53c31724871f8aec2b7f21a7611a720c1f
Royan Harsha <royan@konekt.lk>
2023-10-16 00:25:15 +0530

    Add : Create Station View

--- app/Http/Controllers/StationsController.php
+++ app/Http/Controllers/StationsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\District;
 use Illuminate\Http\Request;
 
 class StationsController extends Controller
@@ -9,14 +10,14 @@ class StationsController extends Controller
     public function index()
     {
         // Index Train Stations
-
         return view('stations.index');
-
     }
 
     public function create()
     {
-
+        // Create Station
+        $districts = District::orderBy('name')->get();
+        return view('stations.create', compact('districts'));
     }
 
     public function store()
