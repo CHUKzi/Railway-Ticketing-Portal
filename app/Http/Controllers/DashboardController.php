4bf05112a38ffad251d6c4a90b44d09c1aa891a9
Royan Harsha <royan@konekt.lk>
2023-10-30 02:18:29 +0530

    Update : User Dashboard And UI Update

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Booking;
+use App\Models\Buyer;
 use App\Models\Station;
 use App\Models\User;
 use Auth;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class DashboardController extends Controller
 {
@@ -19,7 +22,36 @@ class DashboardController extends Controller
         $count_users = User::role('user')->count();
         $count_back_office_staff = User::role(['staff','admin','super admin'])->count();
         $count_stations = Station::count();
+        $count_booking = Booking::where('status', 'active')->count();
 
-        return view('dashboard', compact('count_users', 'count_back_office_staff', 'count_stations'));
+        // Last 7 days of charts
+
+        $endDate = Carbon::now()->endOfDay();
+        $startDate = Carbon::now()->subDays(6);
+
+        $b_data = [['Days', 'Booking']];
+
+        for ($date = $startDate; $date->lte($endDate); $date->addDay()) {
+            $dayName = $date->englishDayOfWeek;
+            $bookingCount = Booking::whereDate('created_at', $date->toDateString())->count();
+            $b_data[] = [$dayName, $bookingCount];
+        }
+
+        $last7_days_booking_data = json_encode($b_data);
+
+        $endDate = Carbon::now()->endOfDay();
+        $startDate = Carbon::now()->subDays(6);
+
+        $p_data = [['Days', 'Payment']];
+
+        for ($date = $startDate; $date->lte($endDate); $date->addDay()) {
+            $dayName = $date->englishDayOfWeek;
+            $paymentsCount = Buyer::whereDate('created_at', $date->toDateString())->count();
+            $p_data[] = [$dayName, $paymentsCount];
+        }
+
+        $last7_days_payment_data = json_encode($p_data);
+
+        return view('dashboard', compact('count_users', 'count_back_office_staff', 'count_stations', 'count_booking', 'last7_days_booking_data', 'last7_days_payment_data'));
     }
 }
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -5,15 +5,12 @@
 @endsection
 
 @section('css')
-
-
     <link rel="stylesheet" type="text/css"
         href="{{ URL::asset('/build/bower_components/datatables.net-bs4/css/dataTables.bootstrap4.min.css') }}">
     <link rel="stylesheet" type="text/css"
         href="{{ URL::asset('/build/assets/pages/data-table/css/buttons.dataTables.min.css') }}">
     <link rel="stylesheet" type="text/css"
         href="{{ URL::asset('/build/bower_components/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css') }}">
-
 @endsection
 
 @section('content')
@@ -48,7 +45,7 @@
                 <div class="page-body">
 
                     <div class="card">
-{{--                         <div class="card-header">
+                        {{--                         <div class="card-header">
                             <h5>Base Style</h5>
                         </div>
  --}}
@@ -56,8 +53,7 @@
 
                             <div class="dt-responsive table-responsive">
 
-                                <table id="base-style"
-                                    class="table table-striped table-bordered nowrap">
+                                <table id="base-style" class="table table-striped table-bordered nowrap">
                                     <thead>
                                         <tr>
                                             <th>ID</th>
@@ -65,33 +61,50 @@
                                             <th>Email</th>
                                             <th>Phone</th>
                                             <th>Credit Points</th>
+                                            <th>Account Status</th>
                                             <th>Last Login</th>
                                             <th>Action</th>
                                         </tr>
                                     </thead>
                                     <tbody>
                                         @foreach ($users as $user)
-                                        <tr>
-                                            <td>{{ $user->id }}</td>
-                                            <td>{{ $user->first_name }}&nbsp;{{ $user->last_name }}</td>
-                                            <td>{{ $user->email }}</td>
-                                            <td>{{ $user->mobile ? : 'N/A'}}</td>
-                                            <td>{{ number_format($user->credit_points, 2) ? : '0.00' }}</td>
-                                            <td>{{ $user->last_login ?: 'Not yet' }}</td>
-                                            <td>
-                                                <div style="display: flex;">
-                                                {{-- <a href="{{ route('users.view', $user->id) }}">
+                                            <tr>
+                                                <td>{{ $user->id }}</td>
+                                                <td>
+                                                    {{ $user->first_name }}&nbsp;{{ $user->last_name }}<br/>
+                                                    @foreach ($user->getRoleNames() as $role)
+                                                        @if ($role === 'staff' || $role === 'admin' || $role === 'super admin')
+                                                            <span class="badge badge-primary">{{ $role }}</span>
+                                                        @endif
+                                                    @endforeach
+                                                </td>
+
+                                                <td>{{ $user->email }}</td>
+                                                <td>{{ $user->mobile ?: 'N/A' }}</td>
+                                                <td>{{ number_format($user->credit_points, 2) ?: '0.00' }}</td>
+                                                <td>
+                                                    @if ($user->email_verified_at)
+                                                        <label class="label label-info">Verified</label>
+                                                    @else
+                                                        <label class="label label-danger">Pedding</label>
+                                                    @endif
+                                                </td>
+                                                <td>{{ $user->last_login ?: 'Not yet' }}</td>
+                                                <td>
+                                                    <div style="display: flex;">
+                                                        {{-- <a href="{{ route('users.view', $user->id) }}">
                                                     <button class="btn btn-mat waves-effect waves-light btn-primary btn-sm">view</button>
                                                 </a> --}}
-                                                <form action="{{ route('users.destroy', $user->id) }}" method="POST">
-                                                    @csrf
-                                                    @method('DELETE')
-                                                    <button type="submit" onclick="return confirm('Are you sure?')" class="btn btn-mat waves-effect waves-light btn-danger btn-sm">Delete</button>
-                                                </form>
-                                                </div>
-                                            </td>
-                                        </tr>
-
+                                                        <form action="{{ route('users.destroy', $user->id) }}"
+                                                            method="POST">
+                                                            @csrf
+                                                            @method('DELETE')
+                                                            <button type="submit" onclick="return confirm('Are you sure?')"
+                                                                class="btn btn-mat waves-effect waves-light btn-danger btn-sm">Delete</button>
+                                                        </form>
+                                                    </div>
+                                                </td>
+                                            </tr>
                                         @endforeach
                                     </tbody>
                                 </table>
@@ -104,7 +117,6 @@
             </div>
         </div>
     </div>
-
 @endsection
 
 
